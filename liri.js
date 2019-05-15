433da3be60bb662cdba356f4365311e95e74678b
Sean Huff <swhufnagel@gmail.com>
2019-05-15 12:25:55 -0600

    bonus done

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var arguments = process.argv;
 
 function searchSong() {
   if (process.argv.length === 3) {
-    query = "The Sign"
+    query = "Ace The Sign"
   }
   spotify.search({ type: 'track', query: query }, function (err, data) {
     if (err) {
@@ -18,10 +18,14 @@ function searchSong() {
     console.log("Track: " + data.tracks.items[0].name);
     console.log("Link: " + data.tracks.items[0].preview_url);
     console.log("Album: " + data.tracks.items[0].album.name);
+    fs.appendFile("log.txt", 
+    "Artist: " + data.tracks.items[0].album.artists[0].name + "\n"
+    + "Track: " + data.tracks.items[0].name + "\n"
+    +  "Link: " + data.tracks.items[0].preview_url + "\n" 
+    + "Album: " + data.tracks.items[0].album.name + "\n", function(err){});
   });
 }
 function searchMovie(){
-  console.log(movie)
     if (process.argv.length === 3) {
       movie = "Mr.Nobody"
     }
@@ -35,6 +39,17 @@ function searchMovie(){
         console.log("Language: " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Cast: " + response.data.Actors);
+
+        fs.appendFile("log.txt", 
+        "Title: " + response.data.Title + "\n"
+        + "Year: " + response.data.Year + "\n"
+        + "IMDB Rating: " + response.data.Ratings[0].Value + "\n"
+        + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
+        + "Country: " + response.data.Country + "\n"
+        + "Language: " + response.data.Language + "\n"
+        + "Plot: " + response.data.Plot + "\n"
+        + "Cast: " + response.data.Actors + "\n"
+    , function(err){});
       }
     );
   }
@@ -43,7 +58,7 @@ function searchConcert(){
     artist = artist.join("+");
   }
   if (process.argv.length === 3) {
-    artists = "Twiddle"
+    artist = "Twiddle"
   }
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
     function (response, err) {
@@ -51,6 +66,12 @@ function searchConcert(){
         console.log(response.data[i].datetime)
         console.log(response.data[i].venue.name)
         console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + "\n")
+
+        fs.appendFile("log.txt", 
+    "Date: " + response.data[i].datetime + "\n"
+    + "Venue: " + response.data[i].venue.name + "\n"
+    +  "Location: " + response.data[i].venue.city + "\n" 
+    , function(err){});
       }
       if (err) {
         console.log("error: " + err);
